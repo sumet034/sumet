c8620ad352628812685338278955c9a1e5e5a824
sumet034 <addzaxs@gmail.com>
2024-05-27 16:47:17 +0700

    NEW Color

--- resources/js/app.js
+++ resources/js/app.js
@@ -17,7 +17,7 @@ function App() {
                                 </button>
                                 <a href="https://flowbite.com" className="flex ml-2 md:mr-24">
                                     <img src="https://flowbite.com/docs/images/logo.svg" className="h-8 mr-3" alt="FlowBite Logo" />
-                                    <span className="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">orders</span>
+                                    <span className="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">order</span>
                                 </a>
                             </div>
                             <div className="flex items-center">
